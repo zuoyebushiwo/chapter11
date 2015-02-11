950bd3d8a22dba3125ab57eeea2b6d960c51e948
zhangxuej <957168943@qq.com>
2015-02-11 09:08:34 +0800

    Change-Id: I0cddc774cc262e26f3c083d2b8973ba8227bf936

--- src/com/baobaotao/dao/TestForumDao.java
+++ src/com/baobaotao/dao/TestForumDao.java
@@ -1,21 +1,17 @@
 package com.baobaotao.dao;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.baobaotao.dao.ForumDao;
 import com.baobaotao.domain.Forum;
-import com.baobaotao.service.BbtForum;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:applicationContext.xml" })
